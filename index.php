9541ee12866184a8e60d3615e74d146dfc989cee
Mian-Aamir <mianaamir8877@gmail.com>
2026-04-21 19:43:31 +0500

    [SCRUM-7][SCRUM-8][SCRUM-9] Add navbar with logo, navigation links and signup button

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QuickPOS - The Last POS System You'll Ever Need</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+
+<!-- NAVBAR -->
+<nav class="navbar">
+    <div class="nav-container">
+        <div class="logo">
+            <i class="fas fa-cash-register"></i>
+            <span>QuickPOS</span>
+        </div>
+        <ul class="nav-links">
+            <li><a href="#features">Features</a></li>
+            <li><a href="#pricing">Pricing</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+        <a href="#" class="btn-signup">Sign Up</a>
+    </div>
+</nav>
+
+</body>
+</html>
